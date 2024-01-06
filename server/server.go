6fe069a3159fd92b044e0ae13d1ae04fd88156cc
mirodavide <filomenodavide.miro@gmail.com>
2024-01-07 00:27:44 +0100

    Fix error server HttpHandler

--- server/server.go
+++ server/server.go
@@ -20,6 +20,7 @@ func (server *Server) NewServer(name string, ip string, port string) {
 }
 
 func (server *Server) Start() {
+	server.handler = new(HttpHandler)
 	server.handler.NewHttpHandler(server.name)
 	fmt.Printf("Starting server at port %s and address %s\n", server.port, server.ip)
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%s", server.ip, server.port), server.handler); err != nil {
